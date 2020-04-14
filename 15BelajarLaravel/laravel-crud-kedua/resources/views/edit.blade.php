2a792bd21177503e7a8851d99905cd40a9e128e5
kinanpermata <kinanti.permat@gmail.com>
2020-04-14 14:15:18 +0700

    jobsheet 10 prak 2 + laporan salah upload di edmodo

---
+++ 15BelajarLaravel/laravel-crud-kedua/resources/views/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('master')
+<!-- Isi title -->
+@section('title', 'Edit Data')
+
+<!-- Isi bagian judul halaman-->
+@section('judul_halaman', 'Edit Data Mahasiswa')
+
+<!-- Isi bagian konten-->
+@section('konten')
+    <a href="/" class="btn btn-danger">Kembali</a>
+    <br/>
+    <br/>
+    <form action="/mahasiswa/update/{{$mahasiswa->id}}" method="POST">
+    {{ csrf_field() }}
+    <input type="hidden" name="id" value="{{$mahasiswa->id}}"> <br>
+    <div class="form-group">
+        <label for="namamhs">Nama</label>
+        <input type="text" class="form-control" required="required" name="namamhs" value="{{$mahasiswa->nama}}"> <br>
+    </div>
+    <div class="form-group">
+        <label for="nimmhs">Nim</label>
+        <input type="number" class="form-control" required="required" name="nimmhs" value="{{$mahasiswa->nim}}"> <br>
+    </div>
+    <div class="form-group">
+        <label for="emailmhs">E-mail</label>
+        <input type="email" class="form-control" required="required" name="emailmhs" value="{{$mahasiswa->email}}"> <br>
+    </div>
+    <div class="form-group">
+        <label for="jurusanmhs">Jurusan</label>
+        <input type="text" class="form-control" required="required" name="jurusanmhs" value="{{$mahasiswa->jurusan}}"> <br>
+    </div>
+    <button type="submit" name="edit" class="btn btn-primary float-right">Simpan Data</button>
+    </form>
+@endsection
