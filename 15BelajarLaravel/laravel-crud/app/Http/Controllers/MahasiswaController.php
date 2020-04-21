3fb014aee99394fc97ca74c00bf2965c596e601a
kinanpermata <kinanti.permat@gmail.com>
2020-04-21 11:16:00 +0700

    jobsheet 12 login

--- 15BelajarLaravel/laravel-crud/app/Http/Controllers/MahasiswaController.php
+++ 15BelajarLaravel/laravel-crud/app/Http/Controllers/MahasiswaController.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         // Mengambil data dari tabel mahasiswa
--- 15BelajarLaravel/laravel-crud/routes/web.php
+++ 15BelajarLaravel/laravel-crud/routes/web.php
@@ -17,7 +17,13 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', 'MahasiswaController@index');
+// Route::get('/', 'MahasiswaController@index');
+
+Route::get('/', function(){
+    return view('welcome');
+});
+
+Route::get('/mahasiswa', 'MahasiswaController@index');
 
 Route::get('/mahasiswa/tambah', 'MahasiswaController@tambah');
 
@@ -30,3 +36,8 @@ Route::get('/mahasiswa/edit/{id}', 'MahasiswaController@edit');
 Route::post('/mahasiswa/update', 'MahasiswaController@update');
 
 Route::get('/mahasiswa/hapus/{id}', 'MahasiswaController@hapus');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/admin/users', 'Admin\UsersController', ['expect' => ['show','create','store']]);
